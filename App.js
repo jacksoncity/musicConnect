3151e1b11eea2d473ba2804764116066c85a3503
jacksoncity <jacksonstokke@gmail.com>
2023-03-28 15:02:07 -0500

    starting the framework via react/js and connecting it through the HTML file

---
+++ App.js
@@ -0,0 +1,34 @@
+export default function App() {
+    return <ConnectAtoA />;
+  }
+
+function PrimaryArtistSearch(){
+    return (
+    <form>
+        <input type="text" placeholder="Enter First Artist" />
+    </form>
+    );
+}
+
+function SecondaryArtistSearch(){
+    return (
+    <form>
+        <input type="text" placeholder="Enter Second Artist" />
+        <label>
+            <input type="checkbox" />
+            {' test '}
+            Use genre instead
+        </label>
+    </form>
+    );
+}
+
+
+function ConnectAtoA() {
+    return (
+        <div>
+            <PrimaryArtistSearch />
+            <SecondaryArtistSearch />
+        </div>
+    );
+}
